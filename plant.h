8ef2c945021d275f81ea571e4780618e19e3b51d
aarun <arunakhil2@gmail.com>
2019-01-07 20:34:31 -0800

    Added plant header file
    Taken from plant.cpp

---
+++ plant.h
@@ -0,0 +1,14 @@
+using namespace std;
+
+class Plant {
+
+    int id, curState, yield, gdd, liveState, deadState;
+    bool living, flowering;
+    int l_states[][]; // see if there is better data structure
+    int d_states[][];
+
+    public:
+        Plant(int id);
+        void transition(int env[]);
+
+    };
