7334899c31f40c58df05f8f463bfc92547677161
Surya Nehra <nehra@usc.edu>
2019-01-14 18:19:39 -0800

    Update plant.cpp

--- plant.cpp
+++ plant.cpp
@@ -9,12 +9,21 @@ Plant::Plant(int id)
 
 
     // Populate states data structure based on database
-
+    liveState = 0;
+    dieState = 0;
     living = true;
     flowering = false;
     dead = false;
 }
 
+bool Plant::checkNeeds(int env[]){
+    if(env[1] >= waterNeeds && env[2] >= nutrientNeeds){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 void Plant::transition(int env[])
 {
     // if env fulfills needs then increment liveState
@@ -22,7 +31,12 @@ void Plant::transition(int env[])
     // if env does not fulfill needs of current state increment deadState
     // kill plant if deadState exceeds threshold
     if(living == 1){
-        
+        if(checkNeeds(env[])){
+            liveState++;
+        }
+        else{
+            dieState++;
+        }
     }
 }
 
