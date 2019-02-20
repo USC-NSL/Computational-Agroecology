4bba5102e7eec45ffbea632397036a30d09bf94a
Surya Nehra <nehra@usc.edu>
2019-02-19 19:28:18 -0800

    Update plant.cc

--- common/plant.cc
+++ common/plant.cc
@@ -1,11 +1,10 @@
 #include "plant.h"
 
 // TODO: Old code for CheckNeeds(), revive as needed and remove comment.
-//       flesh this function out as necessary
-//       assume water and nutrient data are stored in env 1 and 2
-//       respectively
 bool Plant::CheckNeeds(int rainfall, int temperature){
-     if(rainfall >=  type_.minAbsoluteRainfall && temperature >= type_.minAbsoluteTemperature){
+     if(rainfall >= type_.min_absolute_rainfall() && rainfall <= type_.max_absolute_rainfall() && 
+        temperature >= type_.min_absolute_temperature() && 
+        temperature <= type_.max_absolute_temperature()){
          return true;
      }
      else{
@@ -18,15 +17,15 @@ bool Plant::CheckNeeds(int rainfall, int temperature){
 //    // if liveState reaches threshold then advance curState
 //    // if env does not fulfill needs of current state increment deadState
 //    // kill plant if dieState exceeds threshold
-bool Plant::Transition(Maturity new_maturity) {
+void Plant::Transition(Maturity new_maturity) {
   // TODO: Add checks here (e.g., call CheckNeeds()).
+    if(health_ == 0){
+         return;
+    }
     if (CheckNeeds(rainfall, temperature)) {
         maturity_ = new_maturity;
     }
     else {
         health_ --;
     }
-  
-
-  return true;
 }
