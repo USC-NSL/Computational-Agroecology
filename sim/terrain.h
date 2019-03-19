285c8887255b993801245973dce0d1a4abe07c8e
Julia Cordero <jrcorder@usc.edu>
2019-03-18 18:49:24 -0700

    state terrain structure

---
+++ sim/terrain.h
@@ -0,0 +1,16 @@
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+class StateTerrainStructure {
+public:
+    StateTerrainStructure();
+    ~StateTerrainStructure();
+private:
+    int[10000][10000] terrain;
+};
+
+
+
+
+
+#endif
