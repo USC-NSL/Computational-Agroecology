340be9c1e8815edc4c75d6b227933d9ce663d7d9
Surya Nehra <nehra@usc.edu>
2019-03-20 17:41:36 -0700

    soil.h and corn/soy CSV files
    CSVs are formatted as follows
    Name, MinOT,MaxOT,MinAT,MaxAT,MinOR,MaxOR,MinAR,MaxAR,GDDThreshold1,GDDThreshold2,GDDThreshold3,GDDThreshold4,GDDThreshold5,BaseTemp

---
+++ common/soil.h
@@ -0,0 +1,40 @@
+#ifndef AGROECOLOGY_SOIL_H_
+#define AGROECOLOGY_SOIL_H_
+
+class Soil {
+	public:
+		enum class Texture {
+			CLAY,
+			SILT,
+			SAND
+		};
+		
+		Texture texture() const { return texture_; }
+		double pH() const { return pH_; }
+		double salinity() const { return salinity_; }
+		double organic_matter() const { return organic_matter_; }
+		double water_content() const { return water_content_; }
+		double* nutrients() const { return nutrients_; }
+		
+		void addWater(double quantity) { water_content_ += quantity; }
+		void addNitrogen(double quantity) { nutrients_[0] += quantity; }
+		void addPhosphorus(double quantity) { nutrients_[1] += quantity; }
+		void addPotassium(double quantity) { nutrients_[2] += quantity; }
+		void addCalcium(double quantity) { nutrients_[3] += quantity; }
+		void addMagnesium(double quantity) { nutrients_[4] += quantity; }
+		void addSulfur(double quantity) { nutrients_[5] += quantity; }
+		
+	private:
+		Texture texture_;
+		double pH_;
+		double salinity_;
+		double organic_matter_;
+		double water_content_;
+		
+		//contains nitrogen, phosphorus, potassium, calcium, magnesium, and sulfur 
+		//information for the soil, contained in the corresponding indices
+		double nutrients_[6];
+		
+};
+
+#endif
