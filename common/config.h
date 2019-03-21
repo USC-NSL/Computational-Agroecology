fed401694295412efc534f059e2f2b3fee9904c9
Akhil Arun <akhilaru@usc.edu>
2019-03-20 17:54:32 -0700

    config through clang format

--- common/config.h
+++ common/config.h
@@ -1,24 +1,10 @@
 #include "../location.h"
 
-
 class Config {
+ public:
+  Location getLoc() { return loc; }
+  std::vector<PlantType> getPlants() { return plants; }
 
-public:
-
-
-
-	Location getLoc() { return loc;}
-	std::vector<PlantType> getPlants()  {return plants;}
-
-
-private:
-
-
-	Location loc; // latitude and longitude of location
-	std::vector<PlantType> plants; // all available plants to use
-
-
-
-
-
-}
+ private:
+  Location loc_;                   // latitude and longitude of location
+  std::vector<PlantType> plants_;  // all available plants to use
