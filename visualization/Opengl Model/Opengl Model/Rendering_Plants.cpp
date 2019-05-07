6c11b05bd5c115d25905f2bbfc1d49f140bd89eb
Hangjie <hangjiezheng@Hangjies-MacBook-Pro.local>
2019-05-07 12:49:46 -0700

    Opengl Version 3

--- visualization/Opengl Model/Opengl Model/Rendering_Plants.cpp
+++ visualization/Opengl Model/Opengl Model/Rendering_Plants.cpp
@@ -21,7 +21,14 @@ Corn::Corn(int Plant_Pos_X, int Plant_Pos_Y, int Plant_Maturity): Plants("Corn",
 
 void Corn::Rendering(GLdouble baseRadius, GLdouble topRadius, GLdouble height, GLint slices, GLint stacks) {
 	glPushMatrix();
-	glColor3f(1.0, 1.0, 0);
+	GLfloat mat_ambient[] = { 1.0f,	1.0f, 0.0f, 1.0f };
+	GLfloat mat_diffuse[] = { 1.0f,	1.0f, 0.0f, 1.0f };
+	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat mat_shininess[] = { 80.0f };
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
 	glTranslatef(getX(), getY(), 1.0);	
 	gluCylinder(pObj, baseRadius, topRadius, height, slices, stacks);
 	glPopMatrix();
@@ -38,7 +45,14 @@ Squash::Squash(int Plant_Pos_X, int Plant_Pos_Y, int Plant_Maturity) : Plants("S
 
 void Squash::Rendering(GLdouble radius, GLint slices, GLint stacks) {
 	glPushMatrix();
-	glColor3f(0.0, 1.0, 0.0);
+	GLfloat mat_ambient[] = { 0.0f,	1.0f, 0.0f, 1.0f };
+	GLfloat mat_diffuse[] = { 0.0f,	1.0f, 0.0f, 1.0f };
+	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat mat_shininess[] = { 80.0f };
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
 	glTranslatef(getX(), getY(), 1.0);
 	gluSphere(pObj, radius, slices, stacks);	
 	glPopMatrix();
@@ -55,7 +69,14 @@ Bean::Bean(int Plant_Pos_X, int Plant_Pos_Y, int Plant_Maturity) : Plants("Bean"
 
 void Bean::Rendering(GLdouble radius, GLdouble height, GLint slices, GLint stacks) {
 	glPushMatrix();
-	glColor3f(0.0, 0.5, 0.6);
+	GLfloat mat_ambient[] = { 0.0f, 0.5f, 0.6f, 1.0f };
+	GLfloat mat_diffuse[] = { 0.0f,	0.5f, 0.6f, 1.0f };
+	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	GLfloat mat_shininess[] = { 80.0f };
+	glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, mat_ambient);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, mat_diffuse);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, mat_shininess);
 	glTranslatef(getX(), getY(), 1.0);
 	gluCylinder(pObj, 0, radius, height, slices, stacks);
 	glPopMatrix();
--- visualization/Opengl Model/Opengl Model/Rendering_Weather.cpp
+++ visualization/Opengl Model/Opengl Model/Rendering_Weather.cpp
@@ -30,24 +30,17 @@ void Weather::modify_Rain(int state, float dir_x, float dir_y, float dir_z, int 
 }
 
 void Weather::Init() {
-	GLfloat mat_diffuse[] = { 0.5f, 0.5f, 0.5f, 1.0f };
-	GLfloat mat_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-	GLfloat mat_shininess[] = { 80.0f };
 	GLfloat light_position[] = { 0.0f, 0.0f, 100.0f, 0.0f };
 	GLfloat light_ambient[] = { 0.8f, 0.8f, 0.8f, 1.0f };
 	GLfloat light_diffuse[] = { 1.0f, 1.0f, 1.0f, 1.0f };
 	GLfloat Light_Model_Ambient[] = { 0.5f , 0.5f , 0.5f , 1.0f };
+	glShadeModel(GL_SMOOTH);
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, Light_Model_Ambient);
 	glLightModeli(GL_LIGHT_MODEL_LOCAL_VIEWER, GL_TRUE);
 
 	glLightfv(GL_LIGHT0, GL_AMBIENT, light_ambient);
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, light_diffuse);
 	glLightfv(GL_LIGHT0, GL_POSITION, light_position);
-
-	glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, mat_diffuse);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
-	glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
 
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
@@ -56,13 +49,12 @@ void Weather::Init() {
 
 void Weather::Rendering() {
 	if (Sun_State == 1) {
-		glLightfv(GL_LIGHT0, GL_POSITION, Sun_Direction);
-
+		GLfloat light_position[] = { Sun_Direction[0],Sun_Direction[1],Sun_Direction[2], 0.0f };
+		glLightfv(GL_LIGHT0, GL_POSITION, light_position);
 		glEnable(GL_BLEND);
 		glDisable(GL_LIGHTING);
 		glColor4f(1.0f, 1.0f, 1.0f, 0.8f);
 		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
 		glEnable(GL_LIGHTING);
 		glDisable(GL_BLEND);
 	}
