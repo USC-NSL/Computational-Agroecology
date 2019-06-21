dd0e2dfb8d8311b027d38d5d8e4699dee6a9d115
郑杭捷 <zhenghangjie@126.com>
2019-06-21 15:51:06 -0700

    modify strcut info

--- environment/suninfo.h
+++ environment/suninfo.h
@@ -1,8 +1,8 @@
 #ifndef COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_SUNINFO_H_
 #define COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_SUNINFO_H_
 namespace environment {
-	struct SunInfo {
-		double SunAzimuth, SolarAltitude, HourlyIrradiance;
-	};
+struct SunInfo {
+  double SunAzimuth, SolarAltitude, HourlyIrradiance;
+};
 }  // namespace environment
 #endif  // COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_SUNINFO_H_
