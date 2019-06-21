66b9fdecb941275bba2a9f254f8e45abbf9bab5f
郑杭捷 <zhenghangjie@126.com>
2019-06-21 13:33:19 -0700

    modify the member data variables in Sun simulator

--- environment/simulators/sun_simulator.h
+++ environment/simulators/sun_simulator.h
@@ -18,43 +18,43 @@ namespace simulator {
       const std::chrono::system_clock::time_point& time) override;
     //the following function just for test
     struct environment::SunInfo get_sunInfo_for_test(int _year, int _month, int _day, int _hour, double _longitude, double _latitude) {
-      getResult(_year, _month, _day, _hour, _longitude, _latitude);
-      return get_sunInfo();
+		GetResult(_year, _month, _day, _hour, _longitude, _latitude);
+      return GetSunInfo();
     }
   private:
-    struct environment::SunInfo get_sunInfo() const;
+    struct environment::SunInfo GetSunInfo() const;
 
-    void getResult(int _year, int _month, int _day, int _hour, double _longitude, double _latitude);
-    void first_step(int _year, int _month, int _day, int _hour, double _longitude, double _latitude);
-    void second_step();
-    void third_step();
-    void fourth_step();
-    void fifth_step();
-    void sixth_step();
-    double _degree_to_radians(double _degree) const;
-    double _radians_to_degree(double _radians) const;
+    void GetResult(int _year, int _month, int _day, int _hour, double _longitude, double _latitude);
+    void FirstStep(int _year, int _month, int _day, int _hour, double _longitude, double _latitude);
+    void SecondStep();
+    void ThirdStep();
+    void FourthStep();
+    void FifthStep();
+    void SixthStep();
+    double DegreeToRadians(double _degree) const;
+    double RadiansToDegree(double _radians) const;
 
-    const double PI = 3.14159265359;
+    const double kPI = 3.14159265359;
 
     //variables for the first step
     bool is_leapYear;
-    int t_d;
-    double sigma, gama_standard_meridian, th, delta, B, EoT, solar_angel_radians, lamda, beta, alpha;
+    int t_d_;
+    double sigma_, gama_standard_meridian_, th_, delta_, B_, EoT_, lamda_, beta_, alpha_;
 
     //variables for the second step
-    double t_ss, t_sr, DL;
+    double t_ss_, t_sr_, DL_;
 
     //varibales for the third step
-    double Ic, epsilon_0, Ic_prime, a, b, I_et_d, b_0, b_1, I_t_d, s;
+    double Ic_, epsilon_0_, Ic_prime_, a_, b_, I_et_d_, b_0_, b_1_, I_t_d_, s_;
 
     //variables for the fourth step
-    double I_df_d, I_dr_d;
+    double I_df_d_, I_dr_d_;
 
     //variables for the fifth step
-    double psi, A_prime, B_prime, I_t, I_et;
+    double psi_, A_prime_, B_prime_, I_t_, I_et_;
 
     //variables for the sixth step
-    double R, K, I_df, I_dr;
+    double R_, K_, I_df_, I_dr_;
   };
 
 } // namespace simulator
