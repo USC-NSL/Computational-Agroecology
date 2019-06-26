82892ff220e05c3844b0e60231180d13d6a32b33
郑杭捷 <zhenghangjie@126.com>
2019-06-26 12:13:04 -0700

    modify

--- environment/simulators/sun_simulator.cc
+++ environment/simulators/sun_simulator.cc
@@ -116,6 +116,8 @@ void SunSimulator::GetDailyDiffuseIrradiance() {
 }
 
 void SunSimulator::GetHourlyIrradiance() {
+  double a_ = sin(lamda_) * sin(sigma_);
+  double b_ = cos(lamda_) * cos(sigma_);
   double psi_ = kPI * I_t_d_ / kSecsPerMin / kMinsPerHour / kHoursPerDay /
          (a_ * acos(-a_ / b_) + b_ * sqrt(1 - (a_ / b_) * (a_ / b_)));
   double A_prime_ = -b_ * psi_;
