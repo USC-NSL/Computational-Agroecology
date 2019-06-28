782bd99306adf10c7cfaa31285269f20d3861003
郑杭捷 <zhenghangjie@126.com>
2019-06-28 08:55:19 -0700

    modify

--- environment/simulators/photon_simulator/photon_simulator_config.h
+++ environment/simulators/photon_simulator/photon_simulator_config.h
@@ -1,13 +1,18 @@
-/**
- * for all variable definitions
- */
 #ifndef __PHOTON_SIMULATOR_CONFIG_H__
 #define __PHOTON_SIMULATOR_CONFIG_H__
+
+#include "photon/photon.h"
+#include "photon/neighbor.h"
+#include "model/model.h"
 
 namespace simulator{
 
 namespace photonsimulator{
 
+const int kXAXIS = 0;
+const int kYAXIS = 1;
+const int kZAXIS = 2;
+const int kLEAF = 3;
 // floating point precision set by this typedef
 typedef float real_t;
 
