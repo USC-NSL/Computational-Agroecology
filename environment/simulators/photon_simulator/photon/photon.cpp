26402635602a0c36be0221923e5954f1643d153d
郑杭捷 <zhenghangjie@126.com>
2019-07-01 12:13:08 -0700

    modify gitignore

---
+++ environment/simulators/photon_simulator/photon/photon.cpp
@@ -0,0 +1,19 @@
+#include "photon.h"
+
+namespace simulator
+{
+ namespace photonsimulator
+{
+ bool compare_x(const Photon& i, const Photon& j) const {
+  return i.pos.x < j.pos.x;
+ }
+ bool compare_y(const Photon& i, const Photon& j) const {
+  return i.pos.y < j.pos.y;
+ }
+ bool compare_z(const Photon& i, const Photon& j) const {
+  return i.pos.z < j.pos.z;
+ }
+ Photon::Photon(const Vector3& dir, const Vector3& pos, const Vector3& power, const char flag) 
+	 :dir(dir), pos(pos), power(power), flag(flag) {}
+} // namespace photonsimulator
+} // namespace simulator
---
+++ environment/simulators/photon_simulator/photon/photon.h
@@ -0,0 +1,27 @@
+#ifndef COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_SIMULATORS_PHOTON_SIMULATOR_PHOTON_H_
+#define COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_SIMULATORS_PHOTON_SIMULATOR_PHOTON_H_
+
+#include "vectors.h"
+
+namespace simulator
+{
+
+namespace photonsimulator
+{
+
+bool compare_x(const Photon& i, const Photon& j) const;
+bool compare_y(const Photon& i, const Photon& j) const;
+bool compare_z(const Photon& i, const Photon& j) const;
+
+class Photon {
+ public:
+  Vector3 dir, pos, power;
+  char flag;
+  Photon(const Vector3& dir, const Vector3& pos, const Vector3& power, const char flag = 0);
+};
+
+} // namespace photonsimulator
+
+} // namespace simulator
+
+#endif // !COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_SIMULATORS_PHOTON_SIMULATOR_PHOTON_H_
