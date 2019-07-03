30e9ddf820b574916d991dec77ee0d61ae56c089
onethree-13 <onethreefred@gmail.com>
2019-07-03 09:55:13 -0700

    update client.ts add status code

--- threejs/ts/client.ts
+++ threejs/ts/client.ts
@@ -1,12 +1,23 @@
-const {CreateAgentRequest, CreateAgentResponse} = require('./agent_server_pb.js');;
-const {AgentServerClient} = require('./agent_server_grpc_web_pb.js');
+const { CreateAgentRequest, CreateAgentResponse } = require('./agent_server_pb.js');;
+const { AgentServerClient } = require('./agent_server_grpc_web_pb.js');
 
-var createAgentService = new AgentServerClient('http://localhost:8080');
+// Please change IP address and port, according to ./../envoy/envoy.yaml
+var createAgentService = new AgentServerClient('http://192.168.136.128:8080');
 
 var request = new CreateAgentRequest();
 request.setAgentName('Barath');
 request.setEnvironmentName('Happy Farm');
 
-createAgentService.createAgent(request, {}, function(_err: any, _response: any) {
-  // ...
+var call = createAgentService.createAgent(request, {}, function (err: { code: any; message: any; }, response: { getMessage: () => void; }) {
+  if (err) {
+    console.log(err.code);
+    console.log(err.message);
+  } else {
+    console.log(response.getMessage());
+  }
+});
+call.on('status', function (status: { code: any; details: any; metadata: any; }) {
+  console.log("status.code: " + status.code);
+  console.log("status.details: " + status.details);
+  console.log("status.metadata: " + status.metadata);
 });
