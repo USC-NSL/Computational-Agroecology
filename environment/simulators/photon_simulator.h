156c340caf3e80d527b75b557ccac5f53e8d4fc0
郑杭捷 <zhenghangjie@126.com>
2019-07-26 15:25:47 -0700

    add more comments

--- environment/simulators/photon_simulator.h
+++ environment/simulators/photon_simulator.h
@@ -57,6 +57,7 @@ class PhotonSimulator : public Simulator {
 
   // let all photons transmit in the space
   void PhotonsModify();
+  //this function is used to randomize the result of one photon, the parameters are three posibilities
   RadianceResult RussianRoulette(const _462::real_t abr, const _462::real_t ref,
                       const _462::real_t tran);
   void ConstructKDTree(std::vector<Photon> &p, const unsigned int begin,
@@ -66,21 +67,27 @@ class PhotonSimulator : public Simulator {
                      const _462::Vector3 &norm, std::vector<Neighbor> &heap,
                      const unsigned int begin, const unsigned int end,
                      _462::real_t &distance);
+  // return the pixel RGB value
   _462::Vector3 GetPixelColor(const _462::Vector3 &ray_pos,
                               const _462::Vector3 &ray_dir);
+  //return the direction of the certain ray by coordinate x and y
   _462::Vector3 GetRayDir(const int x, const int y, const int scene_length,
                           const int scene_width,
                           const _462::Vector3 &camera_pos,
                           const _462::Vector3 &camera_ctr,
                           const _462::Vector3 &camera_up);
+  //return the pixel color for the ray identified by coord x and y, call GetPixelColor inside
   _462::Vector3 GetRayColor(const int x, const int y, const int scene_length,
                             const int scene_width,
                             const _462::Vector3 &camera_pos,
                             const _462::Vector3 &camera_ctr,
                             const _462::Vector3 &camera_up);
+  //get the reflect direction of the input dir and the normal
   _462::Vector3 GetReflect(const _462::Vector3 &dir, const _462::Vector3 &norm);
+  // get the refract direction of the input dir and the normal
   _462::Vector3 GetRefract(const _462::Vector3 &dir, const _462::Vector3 &norm,
                            _462::real_t coef);
+  //this function will return the Model, Mesh, and Face that is first hitted by certain ray identified by pos and dir
   std::tuple<Model *, Mesh *, Face *> FindFirstIntersect(
       const _462::Vector3 &pos, const _462::Vector3 &dir);
   // write result to environment
