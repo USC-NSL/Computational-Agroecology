1eb5fcb6daa3a0025664a8d9c0a3de4c2ad4eca0
Ralph Chung <ralphchung2005@gmail.com>
2019-08-21 13:31:09 -0700

    Return structs instead of pairs or tuples

--- environment/plant_radiation_info.cc
+++ environment/plant_radiation_info.cc
@@ -6,13 +6,12 @@ namespace environment {
 
 PlantRadiationInfo::PlantRadiationInfo(const double lai,
                                        const SunInfo &sun_info)
-    : sun_info_(sun_info), lai_(lai), k_df_(CalculateKDf()) {
+    : sun_info_(sun_info), total_lai_(lai), k_df_(CalculateKDf()) {
   Update(sun_info_);
 }
 
 void PlantRadiationInfo::Update(const SunInfo &sun_info) {
-  std::tie(I_i_dr_d_, I_i_df_d_) =
-      CalculateInterceptDailyRadiance(sun_info.t_sr_, sun_info.t_ss_);
+  I_i_d_ = CalculateInterceptDailyRadiance(sun_info.t_sr_, sun_info.t_ss_);
   UpdateSolarHour(sun_info);
 }
 
@@ -27,13 +26,17 @@ void PlantRadiationInfo::Update(const int day_of_year, const double t_h) {
 void PlantRadiationInfo::UpdateSolarHour(const SunInfo &sun_info) {
   double k_dr = CalculateKDr(sun_info.solar_elevation());
 
-  std::tie(I_i_dr_, I_i_df_) = CalculateInterceptHourlyRadiance(
-      sun_info.I_dr_, k_dr, sun_info.I_df_, k_df_);
+  I_i_ = CalculateInterceptHourlyRadiance(sun_info.I_dr_, k_dr, sun_info.I_df_,
+                                          k_df_);
 
-  std::tie(I_sunlit_, I_shaded_) =
+  AbsorbedPAR par =
       CalculateAbsorbedHourPAR(sun_info.I_dr_, sun_info.I_df_, k_dr);
+  I_sunlit_ = par.sunlit;
+  I_shaded_ = par.shaded;
 
-  std::tie(lai_sunlit_, lai_shaded_) = CalculateLai(k_dr);
+  Lai lai = CalculateLai(k_dr);
+  lai_sunlit_ = lai.sunlit;
+  lai_shaded_ = lai.shaded;
 }
 
 void PlantRadiationInfo::UpdateSolarHour(const double t_h) {
@@ -55,13 +58,15 @@ double PlantRadiationInfo::CalculateKDr(const double solar_elevation) {
 double PlantRadiationInfo::CalculateKDf() const {
   // Formula [3.29] in book p.65
   // k_df = (1 + 0.1174 * sqrt(L)) / (1 + 0.3732 * sqrt(L))
-  double sqrt_L = std::sqrt(lai_);
+  double sqrt_L = std::sqrt(total_lai_);
   return (1.0f + 0.1174 * sqrt_L) / (1.0f + 0.3732 * sqrt_L);
 }
 
-std::pair<double, double> PlantRadiationInfo::CalculateInterceptHourlyRadiance(
-    const double I_dr, const double k_dr, const double I_df,
-    const double k_df) const {
+PlantRadiationInfo::InterceptRadiance
+PlantRadiationInfo::CalculateInterceptHourlyRadiance(const double I_dr,
+                                                     const double k_dr,
+                                                     const double I_df,
+                                                     const double k_df) const {
   // reflection coefficient
   double p = 0.11f;
   // scatter correction
@@ -69,27 +74,30 @@ std::pair<double, double> PlantRadiationInfo::CalculateInterceptHourlyRadiance(
 
   // Formula [3.8] in book p.58
   // I_i_dr = (1 - p) * I_dr * (1 - exp(-sqrt(α) * k_dr * L))
-  double I_i_dr = (1.0f - p) * I_dr * (1 - std::exp(-sqrt_alpha * k_dr * lai_));
+  double I_i_dr =
+      (1.0f - p) * I_dr * (1 - std::exp(-sqrt_alpha * k_dr * total_lai_));
 
   // Formula [3.32] in book p.66
   // I_i_df = (1 - p) * I_df * (1 - exp(-sqrt(α) * k_df * L))
-  double I_i_df = (1.0f - p) * I_df * (1 - std::exp(-sqrt_alpha * k_df * lai_));
+  double I_i_df =
+      (1.0f - p) * I_df * (1 - std::exp(-sqrt_alpha * k_df * total_lai_));
 
-  return std::make_pair(I_i_dr, I_i_df);
+  return {I_i_dr, I_i_df};
 }
 
-std::pair<double, double> PlantRadiationInfo::CalculateInterceptDailyRadiance(
-    const double t_sr, const double t_ss) {
+PlantRadiationInfo::InterceptRadiance
+PlantRadiationInfo::CalculateInterceptDailyRadiance(const double t_sr,
+                                                    const double t_ss) {
   // TODO: This should be filled in
 
   // It is sure that this function would be used, so having it here just to test
   // the dependency.
   UpdateSolarHour(0);
 
-  return std::pair<double, double>(0, 0);
+  return {0.0, 0.0};
 }
 
-std::pair<double, double> PlantRadiationInfo::CalculateAbsorbedHourPAR(
+PlantRadiationInfo::AbsorbedPAR PlantRadiationInfo::CalculateAbsorbedHourPAR(
     const double I_dr, const double I_df, const double k_dr) const {
   // reflection coefficient
   double P = 0.04;
@@ -101,12 +109,12 @@ std::pair<double, double> PlantRadiationInfo::CalculateAbsorbedHourPAR(
   // Formula [3.1] in book p.52
   // τ_dr = exp(-k_dr * L)
   // L: leaf area index
-  double tau_dr = std::exp(-k_dr * lai_);
+  double tau_dr = std::exp(-k_dr * total_lai_);
 
   // Formula [3.6] in book p.57
   // τ_dr_α = exp(-sqrt(α) * k_dr * L)
   // L: leaf area index
-  double tau_dr_alpha = std::exp(-sqrt_alpha * k_dr * lai_);
+  double tau_dr_alpha = std::exp(-sqrt_alpha * k_dr * total_lai_);
 
   // Formula [3.33] in book p.67
   // Q_p_dr = (1 - p) * tau_dr_alpha * Q_dr
@@ -124,10 +132,10 @@ std::pair<double, double> PlantRadiationInfo::CalculateAbsorbedHourPAR(
   // Q_p_df_bar = (1 - p) * Q_df * (1 - exp(-sqrt(α) * k_df * L)) /
   //              (sqrt(α) * k_df * L)
   double I_p_df_bar = (1.0 - P) * I_df *
-                      (1.0 - std::exp(-sqrt_alpha * k_df_ * lai_)) /
-                      (sqrt_alpha * k_df_ * lai_);
+                      (1.0 - std::exp(-sqrt_alpha * k_df_ * total_lai_)) /
+                      (sqrt_alpha * k_df_ * total_lai_);
 
-  // Formulat [3.38] in book p.68
+  // Formula [3.38] in book p.68
   // Q_sl = α * (k_dr * Q_dr + Q_p_df_bar + Q_p_dr_α)
   double I_sunlit = alpha * (k_dr * I_dr + I_p_df_bar + I_p_dr_alpha);
 
@@ -135,23 +143,23 @@ std::pair<double, double> PlantRadiationInfo::CalculateAbsorbedHourPAR(
   // Q_sh = α * (Q_p_df_bar + Q_p_dr_α)
   double I_shaded = alpha * (I_p_df_bar + I_p_dr_alpha);
 
-  return std::pair<double, double>(I_sunlit, I_shaded);
+  return {I_sunlit, I_shaded};
 }
 
-std::pair<double, double> PlantRadiationInfo::CalculateLai(
+PlantRadiationInfo::Lai PlantRadiationInfo::CalculateLai(
     const double k_dr) const {
   // Formula [3.41] in book p.69
   // L_sl = (1 - exp(-k_dr * L)) / k_dr
   double L_sl = 0.0;
   if (k_dr > 0.0) {
-    L_sl = (1 - std::exp(-k_dr * lai_)) / k_dr;
+    L_sl = (1 - std::exp(-k_dr * total_lai_)) / k_dr;
   }
 
   // Formula [3.42] in book p.69
   // L_sh = L - L_sl
-  double L_sh = lai_ - L_sl;
+  double L_sh = total_lai_ - L_sl;
 
-  return std::pair<double, double>(L_sl, L_sh);
+  return {L_sl, L_sh};
 }
 
 }  // namespace environment
--- environment/plant_radiation_info.h
+++ environment/plant_radiation_info.h
@@ -27,33 +27,35 @@ class PlantRadiationInfo {
 
   // Leaf area index
   // The information in this class is all about this leaf area index
-  const double lai_;
-
-  // A constant that is only related by `lai_`. We store this because it can
-  // be shared by many member functions.
-  const double k_df_;
-
-  // Hourly direct solar radiation intercepted by the canopies
-  double I_i_dr_;
-  // Hourly diffuse solar radiation intercepted by the canopies
-  double I_i_df_;
-
-  // Daily direct solar radiation intercepted by the canopies
-  double I_i_dr_d_;
-  // Daily diffuse solar radiation intercepted by the canopies
-  double I_i_df_d_;
-
-  // The total flux density absorbed by the sunlit leaves
-  double I_sunlit_;
-  // The total flux density absorbed by the shaded leaves
-  double I_shaded_;
-
+  const double total_lai_;
   // Sunlit leaf area index (the total area of sunlit leaves in a unit ground
   // area)
   double lai_sunlit_;
   // Shaded leaf area index (the total area of shaded leaves in a unit ground
   // area)
   double lai_shaded_;
+
+  // A constant that is only related by `lai_`. We store this because it can
+  // be shared by many member functions.
+  const double k_df_;
+
+  // Solar radiation intercepted by the canopies
+  struct InterceptRadiance {
+    // Direct solar radiation intercepted by the canopies
+    double direct;
+    // Diffuse solar radiation intercepted by the canopies
+    double diffuse;
+  };
+  // Hourly direct and diffuse solar radiation intercepted by the canopies
+  InterceptRadiance I_i_;
+
+  // Daily direct and diffuse solar radiation intercepted by the canopies
+  InterceptRadiance I_i_d_;
+
+  // The total flux density absorbed by the sunlit leaves per unit leaf area
+  double I_sunlit_;
+  // The total flux density absorbed by the shaded leaves per unit leaf area
+  double I_shaded_;
 
   // Only update information about the current solar hour according to the
   // provided `sun_info`.
@@ -71,24 +73,36 @@ class PlantRadiationInfo {
 
   // Given direct and diffuse radiation, k_dr, and k_df, calculate the
   // interception of direct and diffuse solar radiation.
-  std::pair<double, double> CalculateInterceptHourlyRadiance(
-      const double I_dr, const double k_dr, const double I_df,
-      const double k_df) const;
+  InterceptRadiance CalculateInterceptHourlyRadiance(const double I_dr,
+                                                     const double k_dr,
+                                                     const double I_df,
+                                                     const double k_df) const;
 
   // Given the solar time of sunrise and sunset, calculate the daily direct and
   // diffuse solar radiation.
-  std::pair<double, double> CalculateInterceptDailyRadiance(const double t_sr,
-                                                            const double t_ss);
+  InterceptRadiance CalculateInterceptDailyRadiance(const double t_sr,
+                                                    const double t_ss);
 
+  struct AbsorbedPAR {
+    // PAR absored by sunlit leaves
+    double sunlit;
+    // PAR absored by shaded leaves
+    double shaded;
+  };
   // Given direct, diffuse radiation, and k_dr, calculate the total flux density
   // absorbed by sunlit leaves and shaded leaves.
-  std::pair<double, double> CalculateAbsorbedHourPAR(const double I_dr,
-                                                     const double I_df,
-                                                     const double k_dr) const;
+  AbsorbedPAR CalculateAbsorbedHourPAR(const double I_dr, const double I_df,
+                                       const double k_dr) const;
 
+  struct Lai {
+    // Sunlit leaf area index
+    double sunlit;
+    // Shaded leaf area index
+    double shaded;
+  };
   // Given k_dr, calculate the sunlit leaf area index and shaded leaf area
   // index.
-  std::pair<double, double> CalculateLai(const double k_dr) const;
+  Lai CalculateLai(const double k_dr) const;
 };
 
 }  // namespace environment
