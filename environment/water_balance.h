66e58b836c0c7a93e868e6f48b0c741ec7e8b6dc
Oliver Eisenberg <oseisenberg@gmail.com>
2019-10-05 21:05:41 -0700

    Fixed comments on water balance

--- environment/water_balance.h
+++ environment/water_balance.h
@@ -1,76 +1,86 @@
-#ifndef COMPUTATIONAL_AGROECOLOGY_WATER_BALANCE_H_
-#define COMPUTATIONAL_AGROECOLOGY_WATER_BALANCE_H_
+#ifndef COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_WATER_BALANCE_H_
+#define COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_WATER_BALANCE_H_
 
 #include <utility>
-
 
 namespace environment {
 
 class WaterBalance {
 
-    // Water content before redistribution (mm)
-    //   length = soil layer thickness (m)
-    //   wc = current water content (m3 m-3)
-    //   wcperc = water percolating from the above soil layer (mm day-1)
-    double WaterContentBeforeRedistribution(double soil_layer_thickness, double water_content, double percolation_from_above);
+  public:
 
-    // Water content after redistribution (mm)
-    //   length = soil layer thickness (m)
-    //   wc = current water content (m3 m-3)
-    double WaterContentAfterRedistribution(double soil_thickness, double water_content);
-
-    // Actual soil evaporation (mm day-1)
-    //   potE = potential soil evaporation (mm day-1)
-    //   e01 and e02 = actual evaporation from layer 1 and 2 (mm day-1)
-    //                 (set within this function)
-    void ActualEvaporation(double potential_evaporation, double &actual_evaporation_layer_1, double &actual_evaporation_layer_2);
-
-    // Actual plant transpiration (mm day-1)
-    //   p = 0.5 or 0.3 for C3 and C4 plants, respectively, represents photosynthesis efficiency
-    //   potT = potential soil transpiration (mm day-1)
-    //   t01 and t02 = actual transpiration from layer 1 and 2 (mm day-1)
-    //                 (set within this function)
-    void ActualTranspiration(double p, double potential_transpiration, 
-            double &actual_transpiration_layer_1, double &actual_transpiration_layer_2);
-
-    // Final volumetric water content (m3 m-3)
-    //   length = soil layer thickness (m)
-    //   vwc = current volumetric water content (m3 m-3)
-    //   ea and ta = actual evaporation and transpiration (mm day-1)
-    //   wcperc0 and wcperc1 = percolation from above and to below
-    //                         (mm day-1)
-    //   Note: wcperc1 is set within this function.
-    double VolumetricWaterContent(double soil_thickness, double volumetric_water_content, 
-            double actual_evaporation, double actual_transpiration, double percolation_from_above, double &percolation_to_below);
-
-    // Final volumetric water content (m3 m-3) for the two soil
-    //   layers. potE and potT are the potential evaporation and
-    //   transpiration (mm day-1).
-    void DailyWaterContent();
+    struct DailyWaterContentReturn {
+        double water_amount_1;
+        double water_amount_1;
+    };
+    
+    // Final volumetric water content (m3 m-3) for the two soil layers. (mm day-1).
+    DailyWaterContentReturn DailyWaterContent(double rainfall, double water_amount_1, double water_amount_2, double dETs, double dETc);
 
     // Fraction of growth reduced due to limited water. potT is potential
     //   transpiration (mm day-1).
-    double GrowthReduction(double potential_transpiration);
+    double GrowthReduction(double potential_transpiration, double water_amount_2);
 
+  private:
 
-    // TODO: Get dETs and dETc from "meteorology.cc" file, from UpdateHourlyNetRadiation (or maybe some other function)
-    double dETs;
-    double dETc;
-    // TODO: Figure out how weather, and rain specifically, is defined in the project
-    double rainfall; // Rainfall on current day (mm)
+    struct ActualEvaporationReturn {
+        double layer_1;
+        double layer_2;
+    };
+
+    struct ActualTranspirationReturn {
+        double layer_1;
+        double layer_2;
+    };
+
+    struct VolumetricWaterContentReturn {
+        double volumetric_water_content;
+        double percolation_to_below;
+    };
+
+    // Water content before redistribution (mm)
+    //   soil_layer_thickness = soil layer thickness (m)
+    //   water_content = current water content (m3 m-3)
+    //   percolation_from_above = water percolating from the above soil layer (mm day-1)
+    double WaterContentBeforeRedistribution(double soil_layer_thickness, double water_content, double percolation_from_above);
+
+    // Water content after redistribution (mm)
+    //   soil_thickness = soil layer thickness (m)
+    //   water_content = current water content (m3 m-3)
+    double WaterContentAfterRedistribution(double soil_thickness, double water_content);
+
+    // Actual soil evaporation (mm day-1)
+    //   potential_evaporation = potential soil evaporation (mm day-1)
+    ActualEvaporationReturn ActualEvaporation(double potential_evaporation, double water_amount_1);
+
+    // Actual plant transpiration (mm day-1)
+    //   photosynthesis_efficiency = 0.5 or 0.3 for C3 and C4 plants, respectively, represents photosynthesis efficiency
+    //   potential_transpiration = potential soil transpiration (mm day-1)
+    ActualTranspirationReturn ActualTranspiration(
+        double photosynthesis_efficiency, double potential_transpiration, double water_amount_2);
+
+    // Final volumetric water content (m3 m-3)
+    //   soil_thickness = soil layer thickness (m)
+    //   volumetric_water_content = current volumetric water content (m3 m-3)
+    //   actual_evaporation and actual_transpiration = actual evaporation and transpiration (mm day-1)
+    //   percolation_from_above = percolation from above (mm day-1)
+    VolumetricWaterContentReturn VolumetricWaterContent(double soil_thickness, double volumetric_water_content, 
+            double actual_evaporation, double actual_transpiration, double percolation_from_above);
 
     // Values based on "input.txt" file from original code
-    const double saturation_point = 0.39;
-    const double wilting_point = 0.07;
-    const double hydraulic_slope = 14.5;
-    const double saturated_hydraulic = 0.19;
-    const int interval = 5;
-    const double depth_1 = 0.02;
-    const double depth_2 = 0.03;
-    double water_amount_1 = 0.2;
-    double water_amount_2 = 0.2;
+    static constexpr double saturation_point = 0.39;
+    static constexpr const double wilting_point = 0.07;
+    static constexpr const double hydraulic_slope = 14.5;
+    static constexpr const double saturated_hydraulic = 0.19;
+    static constexpr const int interval = 5;
+    static constexpr const double depth_1 = 0.02;
+    static constexpr const double depth_2 = 0.03;
+    static constexpr const double photosynthesis_efficiency_c3 = 0.05;
+    static constexpr const double portential_factor = 1000.0 / (2454000.0 * 998.0);
+
+    static constexpr int kMeterToMillimeter = 1000;
 };
 
 }  // namespace environment
 
-#endif  // COMPUTATIONAL_AGROECOLOGY_WATER_BALANCE_H_
+#endif  // COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_WATER_BALANCE_H_
