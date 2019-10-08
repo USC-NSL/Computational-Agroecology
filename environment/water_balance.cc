0631ab732426c71aee9cae11144d1a2d5ebaacac
Oliver Eisenberg <oseisenberg@gmail.com>
2019-10-07 22:53:52 -0700

    Additional syntax fixes

--- environment/water_balance.cc
+++ environment/water_balance.cc
@@ -4,128 +4,134 @@
 
 namespace environment {
 
-double WaterBalance::WaterContentBeforeRedistribution(double soil_layer_thickness, double water_content, double percolation_from_above)
-{
-   // calculates point at which the water can no longer hold more water
-   double water_content_saturation = saturation_point * soil_layer_thickness * kMeterToMillimeter; // for conversion to mm
-   double percolation_to_below = 0.0;
-   double total_water_store = water_content + percolation_from_above;
-   // Percolate water below if the current layer is too saturated
-   // From formula 5.8 on page 109
-   if (total_water_store > water_content_saturation) {
-      percolation_to_below = total_water_store - water_content_saturation;
-   }
-   return(water_content + percolation_from_above - percolation_to_below);
+double WaterBalance::WaterContentBeforeRedistribution(
+    double soil_layer_thickness, double water_content,
+    double percolation_from_above) {
+  // calculates point at which the water can no longer hold more water
+  double water_content_saturation = saturation_point * soil_layer_thickness *
+                                    kMeterToMillimeter;  // for conversion to mm
+  double percolation_to_below = 0.0;
+  double total_water_store = water_content + percolation_from_above;
+  // Percolate water below if the current layer is too saturated
+  // From formula 5.8 on page 109
+  if (total_water_store > water_content_saturation) {
+    percolation_to_below = total_water_store - water_content_saturation;
+  }
+  return (water_content + percolation_from_above - percolation_to_below);
 }
 
-double WaterBalance::WaterContentAfterRedistribution(double soil_thickness, double water_content)
-{
-   double soil_thickness_mm = soil_thickness * kMeterToMillimeter;  // for conversion
-   // From formula 5.3 on page 107
-   // Volumetric WC = volume of water contained in a unit volume of soil
-   double volumetric_water_content = water_content / soil_thickness_mm;        // want unit in m3 m-3
-   // From formula 5.20 on page 114, used to calculate new water content in soil
-   double exp_expression = exp((hydraulic_slope / saturation_point) *
-                     (saturation_point - volumetric_water_content));
-   double val_expression = (hydraulic_slope * saturated_hydraulic * interval) /
-                 (soil_thickness * saturation_point);
-   double log_expression = log(val_expression + exp_expression);
-   double new_water_content = saturation_point - (saturation_point/hydraulic_slope) * log_expression;
-   // Ensure that water content is not negative
-   if (new_water_content < 0) {
-      new_water_content = 0.0;
-   }
-   return (new_water_content * soil_thickness_mm);   // convert back to mm
+double WaterBalance::WaterContentAfterRedistribution(double soil_thickness,
+                                                     double water_content) {
+  double soil_thickness_mm =
+      soil_thickness * kMeterToMillimeter;  // for conversion
+  // From formula 5.3 on page 107
+  // Volumetric WC = volume of water contained in a unit volume of soil
+  double volumetric_water_content =
+      water_content / soil_thickness_mm;  // want unit in m3 m-3
+  // From formula 5.20 on page 114, used to calculate new water content in soil
+  double exp_expression = exp((hydraulic_slope / saturation_point) *
+                              (saturation_point - volumetric_water_content));
+  double val_expression = (hydraulic_slope * saturated_hydraulic * interval) /
+                          (soil_thickness * saturation_point);
+  double log_expression = log(val_expression + exp_expression);
+  double new_water_content =
+      saturation_point - (saturation_point / hydraulic_slope) * log_expression;
+  // Ensure that water content is not negative
+  if (new_water_content < 0) {
+    new_water_content = 0.0;
+  }
+  return (new_water_content * soil_thickness_mm);  // convert back to mm
 }
 
-WaterBalance::ActualEvaporationReturn 
-      WaterBalance::ActualEvaporation(double potential_evaporation, double water_amount_1)
-{
-   // Formula 5.24 on page 115
-   // Calculates reduction of evaporation based on water content, constants from experiments
-   double pow_expression = pow((3.6073 * water_amount_1 / saturated_hydraulic), -9.3172);
-   double reduction_factor = 1.0 / (1.0 + pow_expression);
-   // Formula 5.25 on page on page 115
-   ActualEvaporationReturn actual_evaporation_return = {
+WaterBalance::ActualEvaporationReturn WaterBalance::ActualEvaporation(
+    double potential_evaporation, double water_amount_1) {
+  // Formula 5.24 on page 115
+  // Calculates reduction of evaporation based on water content, constants from
+  // experiments
+  double pow_expression =
+      pow((3.6073 * water_amount_1 / saturated_hydraulic), -9.3172);
+  double reduction_factor = 1.0 / (1.0 + pow_expression);
+  // Formula 5.25 on page on page 115
+  return {
       potential_evaporation * reduction_factor * 0.26,  // layer 1
       potential_evaporation * reduction_factor * 0.74   // layer 2
-   };
-   return actual_evaporation_return;
+  };
 }
 
 WaterBalance::ActualTranspirationReturn WaterBalance::ActualTranspiration(
-      double photosynthesis_efficiency, double potential_transpiration, double water_amount_2)
-{
-   // volumetric water content critical point occurs when reduction factor = 1
-   // Formula 5.29 on page 117
-   double critical_point = wilting_point + 
-                   photosynthesis_efficiency * (saturation_point - wilting_point);
-   double reduction_factor = (water_amount_2 - wilting_point) /
-                       (critical_point - wilting_point);
-   // Reduction factor can't exceed 1.0
-   if (reduction_factor > 1.0) {
-      reduction_factor = 1.0;
-   }
-   ActualTranspirationReturn acutal_transpiration_return = {
-      0.0,
-      potential_transpiration * reduction_factor
-   };
-   return acutal_transpiration_return;
+    double photosynthesis_efficiency, double potential_transpiration,
+    double water_amount_2) {
+  // volumetric water content critical point occurs when reduction factor = 1
+  // Formula 5.29 on page 117
+  double critical_point =
+      wilting_point +
+      photosynthesis_efficiency * (saturation_point - wilting_point);
+  double reduction_factor =
+      (water_amount_2 - wilting_point) / (critical_point - wilting_point);
+  // Reduction factor can't exceed 1.0
+  if (reduction_factor > 1.0) {
+    reduction_factor = 1.0;
+  }
+  ActualTranspirationReturn acutal_transpiration_return = {
+      0.0, potential_transpiration * reduction_factor};
+  return acutal_transpiration_return;
 }
 
 WaterBalance::VolumetricWaterContentReturn WaterBalance::VolumetricWaterContent(
-      double soil_thickness, double volumetric_water_content, double actual_evaporation, 
-      double actual_transpiration, double percolation_from_above)
-{
-   double soil_thickness_mm = soil_thickness * kMeterToMillimeter;     // for conversions
-   double water_content_before = volumetric_water_content * soil_thickness_mm;    // want unit in mm
-   water_content_before = WaterContentBeforeRedistribution(soil_thickness, water_content_before, percolation_from_above);
-   double water_content_after = WaterContentAfterRedistribution(soil_thickness, water_content_before);
-   water_content_after = water_content_after - actual_evaporation - actual_transpiration;
-   if (water_content_after < 0.0) {
-      water_content_after = 0.0;
-   }
-   double percolation_to_below = water_content_before - actual_evaporation - actual_transpiration - water_content_after;  
-   if (percolation_to_below < 0.0) {
-      percolation_to_below = 0.0;
-   }
-   VolumetricWaterContentReturn volumetric_water_content_return = {
-      water_content_after / soil_thickness_mm,
-      percolation_to_below
-   };
-   return volumetric_water_content_return;
+    double soil_thickness, double volumetric_water_content,
+    double actual_evaporation, double actual_transpiration,
+    double percolation_from_above) {
+  double soil_thickness_mm =
+      soil_thickness * kMeterToMillimeter;  // for conversions
+  double water_content_before =
+      volumetric_water_content * soil_thickness_mm;  // want unit in mm
+  water_content_before = WaterContentBeforeRedistribution(
+      soil_thickness, water_content_before, percolation_from_above);
+  double water_content_after =
+      WaterContentAfterRedistribution(soil_thickness, water_content_before);
+  water_content_after =
+      water_content_after - actual_evaporation - actual_transpiration;
+  if (water_content_after < 0.0) {
+    water_content_after = 0.0;
+  }
+  double percolation_to_below = water_content_before - actual_evaporation -
+                                actual_transpiration - water_content_after;
+  if (percolation_to_below < 0.0) {
+    percolation_to_below = 0.0;
+  }
+  return {water_content_after / soil_thickness_mm, percolation_to_below};
 }
 
- // TODO: When calling this function,
- //    Get dETs and dETc from "meteorology.cc" file, from UpdateHourlyNetRadiation (or maybe some other function)
+// TODO: When calling this function,
+//    Get dETs and dETc from "meteorology.cc" file, from
+//    UpdateHourlyNetRadiation (or maybe some other function)
 WaterBalance::DailyWaterContentReturn WaterBalance::DailyWaterContent(
-      double rainfall, double water_amount_1, double water_amount_2, double dETs, double dETc)
-{
-   double potential_evaporation = dETs * portential_factor;
-   double potential_transpiration = dETc * portential_factor;
-   ActualEvaporationReturn actual_evaporation = ActualEvaporation(potential_evaporation, water_amount_1);
-   ActualTranspirationReturn actual_transpiration = ActualTranspiration(
-         photosynthesis_efficiency_c3, potential_transpiration, water_amount_2);
-   VolumetricWaterContentReturn water_content_layer_1 = 
-         VolumetricWaterContent(depth_1, water_amount_1, actual_evaporation.layer_1, 
-               actual_transpiration.layer_1, rainfall);
-   VolumetricWaterContentReturn water_content_layer_2 = 
-         VolumetricWaterContent(depth_2, water_amount_2, actual_evaporation.layer_2, 
-               actual_transpiration.layer_2, water_content_layer_1.percolation_to_below);
-   DailyWaterContentReturn DailyWaterContent = {
-      water_content_layer_1.volumetric_water_content,
-      water_content_layer_2.volumetric_water_content
-   };
-   return DailyWaterContent;
+    double rainfall, double water_amount_1, double water_amount_2, double dETs,
+    double dETc) {
+  double potential_evaporation = dETs * portential_factor;
+  double potential_transpiration = dETc * portential_factor;
+  ActualEvaporationReturn actual_evaporation =
+      ActualEvaporation(potential_evaporation, water_amount_1);
+  ActualTranspirationReturn actual_transpiration = ActualTranspiration(
+      photosynthesis_efficiency_c3, potential_transpiration, water_amount_2);
+  VolumetricWaterContentReturn water_content_layer_1 = VolumetricWaterContent(
+      depth_1, water_amount_1, actual_evaporation.layer_1,
+      actual_transpiration.layer_1, rainfall);
+  VolumetricWaterContentReturn water_content_layer_2 = VolumetricWaterContent(
+      depth_2, water_amount_2, actual_evaporation.layer_2,
+      actual_transpiration.layer_2, water_content_layer_1.percolation_to_below);
+  return {water_content_layer_1.volumetric_water_content,
+          water_content_layer_2.volumetric_water_content};
 }
 
 // Fraction of growth reduced due to limited water. potT is potential
 //   transpiration (mm day-1).
-double WaterBalance::GrowthReduction(double potential_transpiration, double water_amount_2)
-{
-   ActualTranspirationReturn actual_transpiration = ActualTranspiration(
-         photosynthesis_efficiency_c3, potential_transpiration, water_amount_2);
-   return (actual_transpiration.layer_2 / potential_transpiration);       // fraction (0 to 1)
+double WaterBalance::GrowthReduction(double potential_transpiration,
+                                     double water_amount_2) {
+  ActualTranspirationReturn actual_transpiration = ActualTranspiration(
+      photosynthesis_efficiency_c3, potential_transpiration, water_amount_2);
+  return (actual_transpiration.layer_2 /
+          potential_transpiration);  // fraction (0 to 1)
 }
 
 }  // namespace environment
--- environment/water_balance.h
+++ environment/water_balance.h
@@ -6,79 +6,100 @@
 namespace environment {
 
 class WaterBalance {
+ public:
+  // This struct returns the amount of water in both layers of soil.
+  //   For modelling purposes, the soil is treated as having only two layers
+  //   which interact with each other, and hold separate contents.
+  struct DailyWaterContentReturn {
+    double water_amount_1;
+    double water_amount_2;
+  };
 
-  public:
+  // Final volumetric water content (m3 m-3) for the two soil layers. (mm
+  // day-1).
+  DailyWaterContentReturn DailyWaterContent(double rainfall,
+                                            double water_amount_1,
+                                            double water_amount_2, double dETs,
+                                            double dETc);
 
-    struct DailyWaterContentReturn {
-        double water_amount_1;
-        double water_amount_1;
-    };
-    
-    // Final volumetric water content (m3 m-3) for the two soil layers. (mm day-1).
-    DailyWaterContentReturn DailyWaterContent(double rainfall, double water_amount_1, double water_amount_2, double dETs, double dETc);
+  // Fraction of growth reduced due to limited water. potT is potential
+  //   transpiration (mm day-1).
+  double GrowthReduction(double potential_transpiration, double water_amount_2);
 
-    // Fraction of growth reduced due to limited water. potT is potential
-    //   transpiration (mm day-1).
-    double GrowthReduction(double potential_transpiration, double water_amount_2);
+ private:
+  // This struct describes the amount of water that is evaporated from each
+  //   layer of the soil, since depths affects how much surface area is exposed.
+  //   Actual evaporation is calculated by multiplying the potential evaporation
+  //   by some reduction factor.
+  struct ActualEvaporationReturn {
+    double layer_1;
+    double layer_2;
+  };
 
-  private:
+  // This struct describes the amount of water that is transpired from each
+  //   layer of the soil, since depths affects how much surface area is exposed.
+  //   Transpiration is the process of water loss from plants through stomata.
+  struct ActualTranspirationReturn {
+    double layer_1;
+    double layer_2;
+  };
 
-    struct ActualEvaporationReturn {
-        double layer_1;
-        double layer_2;
-    };
+  struct VolumetricWaterContentReturn {
+    double volumetric_water_content;
+    double percolation_to_below;
+  };
 
-    struct ActualTranspirationReturn {
-        double layer_1;
-        double layer_2;
-    };
+  // Water content before redistribution (mm)
+  //   soil_layer_thickness = soil layer thickness (m)
+  //   water_content = current water content (m3 m-3)
+  //   percolation_from_above = water percolating from the above soil layer (mm
+  //   day-1)
+  double WaterContentBeforeRedistribution(double soil_layer_thickness,
+                                          double water_content,
+                                          double percolation_from_above);
 
-    struct VolumetricWaterContentReturn {
-        double volumetric_water_content;
-        double percolation_to_below;
-    };
+  // Water content after redistribution (mm)
+  //   soil_thickness = soil layer thickness (m)
+  //   water_content = current water content (m3 m-3)
+  double WaterContentAfterRedistribution(double soil_thickness,
+                                         double water_content);
 
-    // Water content before redistribution (mm)
-    //   soil_layer_thickness = soil layer thickness (m)
-    //   water_content = current water content (m3 m-3)
-    //   percolation_from_above = water percolating from the above soil layer (mm day-1)
-    double WaterContentBeforeRedistribution(double soil_layer_thickness, double water_content, double percolation_from_above);
+  // Actual soil evaporation (mm day-1)
+  //   potential_evaporation = potential soil evaporation (mm day-1)
+  ActualEvaporationReturn ActualEvaporation(double potential_evaporation,
+                                            double water_amount_1);
 
-    // Water content after redistribution (mm)
-    //   soil_thickness = soil layer thickness (m)
-    //   water_content = current water content (m3 m-3)
-    double WaterContentAfterRedistribution(double soil_thickness, double water_content);
+  // Actual plant transpiration (mm day-1)
+  //   photosynthesis_efficiency = 0.5 or 0.3 for C3 and C4 plants,
+  //   respectively, represents photosynthesis efficiency
+  //   potential_transpiration = potential soil transpiration (mm day-1)
+  ActualTranspirationReturn ActualTranspiration(
+      double photosynthesis_efficiency, double potential_transpiration,
+      double water_amount_2);
 
-    // Actual soil evaporation (mm day-1)
-    //   potential_evaporation = potential soil evaporation (mm day-1)
-    ActualEvaporationReturn ActualEvaporation(double potential_evaporation, double water_amount_1);
+  // Final volumetric water content (m3 m-3)
+  //   soil_thickness = soil layer thickness (m)
+  //   volumetric_water_content = current volumetric water content (m3 m-3)
+  //   actual_evaporation and actual_transpiration = actual evaporation and
+  //   transpiration (mm day-1) percolation_from_above = percolation from above
+  //   (mm day-1)
+  VolumetricWaterContentReturn VolumetricWaterContent(
+      double soil_thickness, double volumetric_water_content,
+      double actual_evaporation, double actual_transpiration,
+      double percolation_from_above);
 
-    // Actual plant transpiration (mm day-1)
-    //   photosynthesis_efficiency = 0.5 or 0.3 for C3 and C4 plants, respectively, represents photosynthesis efficiency
-    //   potential_transpiration = potential soil transpiration (mm day-1)
-    ActualTranspirationReturn ActualTranspiration(
-        double photosynthesis_efficiency, double potential_transpiration, double water_amount_2);
+  // Values based on "input.txt" file from original code
+  static constexpr double saturation_point = 0.39;
+  static constexpr double wilting_point = 0.07;
+  static constexpr double hydraulic_slope = 14.5;
+  static constexpr double saturated_hydraulic = 0.19;
+  static constexpr int interval = 5;
+  static constexpr double depth_1 = 0.02;
+  static constexpr double depth_2 = 0.03;
+  static constexpr double photosynthesis_efficiency_c3 = 0.05;
+  static constexpr double portential_factor = 1000.0 / (2454000.0 * 998.0);
 
-    // Final volumetric water content (m3 m-3)
-    //   soil_thickness = soil layer thickness (m)
-    //   volumetric_water_content = current volumetric water content (m3 m-3)
-    //   actual_evaporation and actual_transpiration = actual evaporation and transpiration (mm day-1)
-    //   percolation_from_above = percolation from above (mm day-1)
-    VolumetricWaterContentReturn VolumetricWaterContent(double soil_thickness, double volumetric_water_content, 
-            double actual_evaporation, double actual_transpiration, double percolation_from_above);
-
-    // Values based on "input.txt" file from original code
-    static constexpr double saturation_point = 0.39;
-    static constexpr const double wilting_point = 0.07;
-    static constexpr const double hydraulic_slope = 14.5;
-    static constexpr const double saturated_hydraulic = 0.19;
-    static constexpr const int interval = 5;
-    static constexpr const double depth_1 = 0.02;
-    static constexpr const double depth_2 = 0.03;
-    static constexpr const double photosynthesis_efficiency_c3 = 0.05;
-    static constexpr const double portential_factor = 1000.0 / (2454000.0 * 998.0);
-
-    static constexpr int kMeterToMillimeter = 1000;
+  static constexpr int kMeterToMillimeter = 1000;
 };
 
 }  // namespace environment
