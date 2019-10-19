439efc4d53cace182476a7ffcfb4aaf46c3012ff
Oliver Eisenberg <oseisenberg@gmail.com>
2019-10-19 14:04:49 -0700

    Fixes from Ralph's first comments

--- environment/photosynthesis.cc
+++ environment/photosynthesis.cc
@@ -10,13 +10,14 @@ double Photosynthesis::Q10TemperatureSensitivity(
     double michaelis_menten_constant_c25, double q10_temperature_sensitivity,
     double leaf_temperature) {
   return (michaelis_menten_constant_c25 *
-          pow(q10_temperature_sensitivity, (leaf_temperature - 25.0) / 10.0));
+          pow(q10_temperature_sensitivity,
+              (leaf_temperature - kTemperatureScalar25) / 10.0));
 }
 
 double Photosynthesis::RubiscoCapacityRate(double leaf_temperature) {
   // From section 6.4.1 on page 129
   double expression = 1.0 + exp(0.128 * (leaf_temperature - 40.0));
-  return Q10TemperatureSensitivity(rubisco_capacity_rate, 2.4,
+  return Q10TemperatureSensitivity(kRubiscoCapacityRate, 2.4,
                                    leaf_temperature) /
          expression;
 }
@@ -24,8 +25,8 @@ double Photosynthesis::RubiscoCapacityRate(double leaf_temperature) {
 double Photosynthesis::CO2CompensationPoint(double leaf_temperature) {
   // From formula 6.19 on page 132
   double sensitivity =
-      Q10TemperatureSensitivity(specificity_factor, 0.57, leaf_temperature);
-  return (o2_concentration_in_air * 0.5 / sensitivity);
+      Q10TemperatureSensitivity(kSpecificityFactor, 0.57, leaf_temperature);
+  return (kO2ConcentrationInAir * 0.5 / sensitivity);
 }
 
 double Photosynthesis::PhotosynthesisLimitedByLight(double par,
@@ -33,7 +34,7 @@ double Photosynthesis::PhotosynthesisLimitedByLight(double par,
                                                     double co2_concentration) {
   // From formula 6.25 on page 133
   double co2_compensation_point = CO2CompensationPoint(leaf_temperature);
-  double numerator = absorption_fraction * quantum_yield * par *
+  double numerator = kAbsorptionFraction * kQuantumYield * par *
                      (co2_concentration - co2_compensation_point);
   double denominator = co2_concentration + 2.0 * co2_compensation_point;
   return (numerator / denominator);
@@ -43,11 +44,11 @@ double Photosynthesis::PhotosynthesisLimitedByRubisco(
     double leaf_temperature, double co2_concentration) {
   // From formula 6.15 on page 129
   double michaelis_menten_co2 = Q10TemperatureSensitivity(
-      michaelis_menten_co2_25_degrees, 2.1, leaf_temperature);
+      kMichaelisMentenCo2Degrees25, 2.1, leaf_temperature);
   double michaelis_menten_o2 = Q10TemperatureSensitivity(
-      michaelis_menten_o2_25_degrees, 1.2, leaf_temperature);
+      kMichaelisMentenO2Degrees25, 1.2, leaf_temperature);
   double dKm = michaelis_menten_co2 *
-               (1.0 + o2_concentration_in_air / michaelis_menten_o2);
+               (1.0 + kO2ConcentrationInAir / michaelis_menten_o2);
   double numerator =
       RubiscoCapacityRate(leaf_temperature) *
       (co2_concentration - CO2CompensationPoint(leaf_temperature));
@@ -63,9 +64,9 @@ double Photosynthesis::PhotosynthesisLimitedBySucroseSink(
 
 double Photosynthesis::LeafPhotosynthesis(double par, double leaf_temperature) {
   double rubisco_limited_value = PhotosynthesisLimitedByRubisco(
-      leaf_temperature, internal_co2_concentration);
+      leaf_temperature, kInternalCo2Concentration);
   double light_limited_value = PhotosynthesisLimitedByLight(
-      par, leaf_temperature, internal_co2_concentration);
+      par, leaf_temperature, kInternalCo2Concentration);
   double sucrose_sink_limited_value =
       PhotosynthesisLimitedBySucroseSink(leaf_temperature);
 
@@ -80,10 +81,8 @@ double Photosynthesis::GrossCanopyPhotosynthesis(double local_solar_hour,
       absorbedHourParReturn = PlantRadiation::CalculateAbsorbedHourPAR(
           0, 0,
           0);  // TODO: Figure Out What Values to put here as parameters!!!!
-  double sunlit =
-      absorbedHourParReturn.sunlit * conversion_factor_to_umol_m2_s1;
-  double shaded =
-      absorbedHourParReturn.shaded * conversion_factor_to_umol_m2_s1;
+  double sunlit = absorbedHourParReturn.sunlit * kConversionFactorToUmolM2S1;
+  double shaded = absorbedHourParReturn.shaded * kConversionFactorToUmolM2S1;
   // leaf assimilation:
   double photosynthesis_sunlit = LeafPhotosynthesis(sunlit, leaf_temperature);
   double photosynthesis_shaded = LeafPhotosynthesis(shaded, leaf_temperature);
--- environment/photosynthesis.h
+++ environment/photosynthesis.h
@@ -9,12 +9,12 @@ class Photosynthesis {
   //   (umol CO2 m-2 s-1).
   //   th (local solar time, hour), lai (leaf area index, m2 m-2),
   //   lftemp (leaf temperature, deg. C)
-  double GrossCanopyPhotosynthesis(double local_solar_hour,
-                                   double leaf_temperature);
+  static double GrossCanopyPhotosynthesis(double local_solar_hour,
+                                          double leaf_temperature);
 
   // Daily gross canopy photosynthesis (per unit ground area)
   //   (umol CO2 m-2 day-1)
-  double DailyGrossCanopyPhotosynthesis();
+  static double DailyGrossCanopyPhotosynthesis();
 
  private:
   // Changes the given model parameter based on current temperature.
@@ -23,50 +23,52 @@ class Photosynthesis {
   //   temperature sensitivity of an enzymatic reaction rate due to increase in
   //   temperature by 10 degrees celsisu, leaf_temperature (leaf temperature,
   //   deg. C)
-  double Q10TemperatureSensitivity(double michaelis_menten_constant_25_degrees,
-                                   double q10_temperature_sensitivity,
-                                   double leaf_temperature);
+  static double Q10TemperatureSensitivity(
+      double michaelis_menten_constant_25_degrees,
+      double q10_temperature_sensitivity, double leaf_temperature);
 
   // Rubisco capacity rate (umol m-2 s-1)
   //   lftemp (leaf temperature, deg. C)
-  double RubiscoCapacityRate(double leaf_temperature);
+  static double RubiscoCapacityRate(double leaf_temperature);
 
   // CO2 compensation point (umol mol-1)
   //   lftemp (leaf temperature, deg. C)
-  double CO2CompensationPoint(double leaf_temperature);
+  static double CO2CompensationPoint(double leaf_temperature);
 
   // Gross photosynthesis limited by light (umol m-2 s-1)
   //   par (PAR, umol m-2 s-1), lftemp (leaf temperature, deg. C),
   //   ci (intercellular CO2 concentration, umol mol-1)
-  double PhotosynthesisLimitedByLight(double par, double leaf_temperature,
-                                      double co2_concentration);
+  static double PhotosynthesisLimitedByLight(double par,
+                                             double leaf_temperature,
+                                             double co2_concentration);
 
   // Gross photosynthesis limited by Rubisco capacity (umol m-2 s-1)
   //   lftemp (leaf temperature, deg. C),
   //   ci (intercellular CO2 concentration, umol mol-1)
-  double PhotosynthesisLimitedByRubisco(double leaf_temperature,
-                                        double co2_concentration);
+  static double PhotosynthesisLimitedByRubisco(double leaf_temperature,
+                                               double co2_concentration);
 
   // Gross photosynthesis limited by sucrose sink (umol m-2 s-1)
   //   lftemp (leaf temperature, deg. C)
-  double PhotosynthesisLimitedBySucroseSink(double leaf_temperature);
+  static double PhotosynthesisLimitedBySucroseSink(double leaf_temperature);
 
   // Gross leaf photosynthesis (umol m-2 s-1)
   //   par (PAR, umol m-2 s-1), lftemp (leaf temperature, deg. C),
-  double LeafPhotosynthesis(double par, double leaf_temperature);
+  static double LeafPhotosynthesis(double par, double leaf_temperature);
 
-  static constexpr double rubisco_capacity_rate = 200.0;
-  static constexpr double specificity_factor = 2600.0;
-  static constexpr double o2_concentration_in_air = 210000.0;
-  static constexpr double quantum_yield = 0.06;
-  static constexpr double absorption_fraction = 0.8;
-  static constexpr double michaelis_menten_co2_25_degrees =
+  static constexpr double kRubiscoCapacityRate = 200.0;
+  static constexpr double kSpecificityFactor = 2600.0;
+  static constexpr double kO2ConcentrationInAir = 210000.0;
+  static constexpr double kQuantumYield = 0.06;
+  static constexpr double kAbsorptionFraction = 0.8;
+  static constexpr double kMichaelisMentenCo2Degrees25 =
       300.0;  // michaelis menten constant for co2 at 25 degrees celsius
-  static constexpr double michaelis_menten_o2_25_degrees =
+  static constexpr double kMichaelisMentenO2Degrees25 =
       300000.0;  // michaelis menten constant for o2 at 25 degrees celsius
-  static constexpr double internal_co2_concentration = 245.0;  // (umol mol-1)
+  static constexpr double kInternalCo2Concentration = 245.0;  // (umol mol-1)
 
-  static constexpr double conversion_factor_to_umol_m2_s1 = 4.5;
+  static constexpr double kConversionFactorToUmolM2S1 = 4.5;
+  static constexpr double kTemperatureScalar25 = 25.0;
 };
 
 }  // namespace environment
