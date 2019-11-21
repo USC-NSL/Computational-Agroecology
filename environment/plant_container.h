32e42f24e93528a6080b2318b2cc8cf050ef367e
Cheng-Yu Chung <chengyuc@usc.edu>
2019-11-21 02:46:09 -0800

    Fixed iterators

--- environment/plant_container.h
+++ environment/plant_container.h
@@ -1,7 +1,6 @@
 #ifndef COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_PLANTCONTAINER_H_
 #define COMPUTATIONAL_AGROECOLOGY_ENVIRONMENT_PLANTCONTAINER_H_
 
-#include <iterator>
 #include <memory>
 #include <string>
 
@@ -15,16 +14,16 @@ namespace environment {
 class PlantContainer {
  public:
   // Types
-  class iterator;
-  class const_iterator;
-  typedef std::reverse_iterator<iterator> reverse_iterator;
-  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
-  typedef std::unique_ptr<Plant> &reference;
-  typedef const std::unique_ptr<Plant> &const_reference;
-  typedef std::unique_ptr<Plant> *pointer;
-  typedef const std::unique_ptr<Plant> *const_pointer;
-  typedef std::unique_ptr<Plant> value_type;
-  typedef std::vector<std::unique_ptr<Plant>>::size_type size_type;
+  using iterator = std::vector<std::unique_ptr<Plant>>::iterator;
+  using const_iterator = std::vector<std::unique_ptr<Plant>>::const_iterator;
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+  using reference = std::unique_ptr<Plant> &;
+  using const_reference = const std::unique_ptr<Plant> &;
+  using pointer = std::unique_ptr<Plant> *;
+  using const_pointer = const std::unique_ptr<Plant> *;
+  using value_type = std::unique_ptr<Plant>;
+  using size_type = std::vector<std::unique_ptr<Plant>>::size_type;
 
   PlantContainer() : plants_(), kdtree_(){};
 
@@ -66,77 +65,6 @@ class PlantContainer {
 
   std::vector<std::unique_ptr<Plant>> plants_;
   std::unique_ptr<KDTree> kdtree_;
-};
-
-class PlantContainer::iterator
-    : public std::iterator<std::bidirectional_iterator_tag,
-                           std::unique_ptr<Plant>> {
- public:
-  iterator() : it_() {}
-  iterator(const std::vector<std::unique_ptr<Plant>>::iterator &it) : it_(it) {}
-  reference operator*() const { return *it_; }
-  pointer operator->() const { return &(*it_); }
-  iterator &operator++() {
-    ++it_;
-    return *this;
-  }
-  iterator operator++(int) {
-    iterator ret = *this;
-    ++(*this);
-    return ret;
-  }
-  iterator &operator--() {
-    --it_;
-    return *this;
-  }
-  iterator operator--(int) {
-    iterator ret = *this;
-    --(*this);
-    return ret;
-  }
-  bool operator==(const iterator &rhs) const { return it_ == rhs.it_; }
-  bool operator!=(const iterator &rhs) const { return it_ != rhs.it_; }
-
- private:
-  friend class const_iterator;
-  std::vector<std::unique_ptr<Plant>>::iterator it_;
-};
-
-class PlantContainer::const_iterator
-    : public std::iterator<std::bidirectional_iterator_tag,
-                           std::unique_ptr<Plant>> {
- public:
-  // implicit conversion from iterator to const_iterator
-  const_iterator(const PlantContainer::iterator &it) : it_(it.it_) {}
-
-  const_iterator() : it_() {}
-  const_iterator(const std::vector<std::unique_ptr<Plant>>::const_iterator &it)
-      : it_(it) {}
-  const_reference operator*() const { return *it_; }
-  const_pointer operator->() const { return &(*it_); }
-  const_iterator &operator++() {
-    ++it_;
-    return *this;
-  }
-  const_iterator operator++(int) {
-    const_iterator ret = *this;
-    ++(*this);
-    return ret;
-  }
-  const_iterator &operator--() {
-    --it_;
-    return *this;
-  }
-  const_iterator operator--(int) {
-    const_iterator ret = *this;
-    --(*this);
-    return ret;
-  }
-  bool operator==(const const_iterator &rhs) const { return it_ == rhs.it_; }
-  bool operator!=(const const_iterator &rhs) const { return it_ != rhs.it_; }
-
- private:
-  std::vector<std::unique_ptr<Plant>>::const_iterator it_;
 };
 
 }  // namespace environment
